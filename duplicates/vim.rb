853af7e458ae4e6596038d0282aadb694c3fea60
Philippe GONCALVES <github@feeloo007.com>
2011-08-21 05:53:15 +0300

    Ajout de with-python-config-dir pour forcer l'utilisation la version de python HOMEBREW

--- duplicates/vim.rb
+++ duplicates/vim.rb
@@ -18,6 +18,7 @@ class Vim < Formula
                           "--enable-multibyte",
                           "--with-tlib=ncurses",
                           "--enable-pythoninterp",
+                          "--with-python-config-dir=/usr/local/Cellar/python/2.7.2/lib/python2.7/config/",
                           "--enable-rubyinterp",
                           "--with-features=huge"
     system "make"
